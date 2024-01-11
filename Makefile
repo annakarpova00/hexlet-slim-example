94c0a9958023d205e1a354bb4b605d44ec3e7833
Anna Karpova <annakarpova@MacBook-Air-Anna.local>
2024-01-11 16:32:58 +0300

    add renderer

--- Makefile
+++ Makefile
@@ -1,4 +1,2 @@
 start:
-	php -S localhost:8081 -t public public/index.php
-
-	
+	/opt/homebrew/Cellar/php/8.3.1/bin/php -S localhost:8081 -t public public/index.php
--- public/index.php
+++ public/index.php
@@ -2,8 +2,15 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-$app = AppFactory::create();
+$container = new Container();
+$container->set('renderer', function () {
+    // Параметром передается базовая директория, в которой будут храниться шаблоны
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
@@ -12,4 +19,42 @@ $app->get('/', function ($request, $response) {
     // Благодаря пакету slim/http этот же код можно записать короче
     // return $response->write('Welcome to Slim!');
 });
+
+$app->post('/users', function ($request, $response) {
+    return $response->withStatus(302);
+});
+
+$app->get('/courses/{id}', function ($request, $response, array $args) {
+    $id = $args['id'];
+    return $response->write("Course id: {$id}");
+});
+$app->get('/companies/{id}', callable: function($request, $response, array $args){
+    $id = (int) $args['id'];
+    $companies = [
+        ['id' => 4, 'name'=> 'IMA'],
+        ['id' => 2, 'name'=> 'Severstal'],
+        ['id' => 8, 'name'=> 'Fosagro']
+    ];
+    foreach ($companies as $elem){
+        if ($id === $elem['id']) {
+            return $response->write(json_encode($elem));
+        }
+    }
+
+    return $response->withStatus(404) ->write('Page not found');
+
+});
+
+$app->get('/users/{id}', function ($request, $response, $args) {
+    $params = [
+        'id' => $args['id'],
+        'nickname' => 'user-' . $args['id'],
+        'message' => 'Здравствуй, любовь моя -'. $args['id'],
+    ];
+    // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации
+    // $this доступен внутри анонимной функции благодаря https://php.net/manual/ru/closure.bindto.php
+    // $this в Slim это контейнер зависимостей
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+});
+
 $app->run();
